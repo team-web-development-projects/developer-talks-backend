371b44d52717b4be6de5bf9ee8b9222a1ca4ce17
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-04-27 21:15:52 +0900

    fix: token 페이로드 sub에 userid 넣던것을 oauth 때문에 email로 바꿈

--- src/main/java/com/dtalks/dtalks/user/service/UserDetailsService.java
+++ src/main/java/com/dtalks/dtalks/user/service/UserDetailsService.java
@@ -5,4 +5,5 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserDetailsService {
     UserDetails loadUserByUserid(String userid) throws UsernameNotFoundException;
+    UserDetails loadUserByEmail(String email) throws UsernameNotFoundException;
 }
