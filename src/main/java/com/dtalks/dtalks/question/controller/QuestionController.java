1b5725a5b7e2d915db38e5c33a76a8b25a1c5c67
taehoon <xogns1514@gmail.com>
2023-05-01 14:40:06 +0900

    Question 구현

---
+++ src/main/java/com/dtalks/dtalks/question/controller/QuestionController.java
@@ -0,0 +1,52 @@
+package com.dtalks.dtalks.question.controller;
+
+import com.dtalks.dtalks.question.dto.QuestionDto;
+import com.dtalks.dtalks.question.dto.QuestionResponseDto;
+import com.dtalks.dtalks.question.service.QuestionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/questions")
+public class QuestionController {
+    private QuestionService questionService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionResponseDto> searchById(@PathVariable Long id) {
+        return ResponseEntity.ok(questionService.searchById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<QuestionResponseDto>> searchAll(@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(questionService.searchAllQuestion(pageable));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<QuestionResponseDto>> searchQuestions(@RequestParam String keyword, @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.ok(questionService.searchQuestions(keyword, pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> createQuestion(@Valid @RequestBody QuestionDto questionDto, @AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(questionService.createQuestion(questionDto, userDetails));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Long> updateQuestion(@Valid @RequestBody QuestionDto questionDto, @PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(questionService.updateQuestion(id, questionDto, userDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteQuestion(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
+        questionService.deleteQuestion(id, userDetails);
+    }
+}
---
+++ src/main/java/com/dtalks/dtalks/question/service/QuestionServiceImpl.java
@@ -0,0 +1,95 @@
+package com.dtalks.dtalks.question.service;
+
+import com.dtalks.dtalks.exception.ErrorCode;
+import com.dtalks.dtalks.exception.exception.PermissionNotGrantedException;
+import com.dtalks.dtalks.exception.exception.PostNotFoundException;
+import com.dtalks.dtalks.question.dto.QuestionDto;
+import com.dtalks.dtalks.question.dto.QuestionResponseDto;
+import com.dtalks.dtalks.question.entity.Question;
+import com.dtalks.dtalks.question.repository.QuestionRepository;
+import com.dtalks.dtalks.user.entity.User;
+import com.dtalks.dtalks.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionServiceImpl implements QuestionService {
+
+    private final UserRepository userRepository;
+    private final QuestionRepository questionRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public QuestionResponseDto searchById(Long id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isEmpty()) {
+            throw new PostNotFoundException(ErrorCode.POST_NOT_FOUND_ERROR, "해당하는 질문글이 존재하지 않습니다. ");
+        }
+        return QuestionResponseDto.toDto(question.get());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<QuestionResponseDto> searchAllQuestion(Pageable pageable) {
+        Page<Question> questionPage = questionRepository.findAllByOrderByIdDesc(pageable);
+        return questionPage.map(QuestionResponseDto::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<QuestionResponseDto> searchQuestions(String keyword, Pageable pageable) {
+        Page<Question> questionPage = questionRepository.findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(keyword, keyword, pageable);
+        if (!questionPage.isEmpty()) {
+            throw new PostNotFoundException(ErrorCode.POST_NOT_FOUND_ERROR, "해당하는 질문글이 존재하지 않습니다. ");
+        }
+        return questionPage.map(QuestionResponseDto::toDto);
+    }
+
+    @Override
+    @Transactional
+    public Long createQuestion(QuestionDto questionDto, UserDetails userDetails) {
+        User user = userRepository.getByNickname(userDetails.getUsername());
+        Question question = Question.toEntity(questionDto, user);
+        questionRepository.save(question);
+        return question.getId();
+    }
+
+    @Override
+    @Transactional
+    public Long updateQuestion(Long questionId, QuestionDto questionDto, UserDetails userDetails) {
+        User user = userRepository.getByNickname(userDetails.getUsername());
+        Optional<Question> optionalQuestion = questionRepository.findById(questionId);
+        if (optionalQuestion.isEmpty()) {
+            throw new PostNotFoundException(ErrorCode.POST_NOT_FOUND_ERROR, "해당하는 질문글이 존재하지 않습니다. ");
+        }
+        Question question = optionalQuestion.get();
+        String userid = question.getUser().getUserid();
+        if (!user.equals(userDetails.getUsername())) {
+            throw new PermissionNotGrantedException(ErrorCode.PERMISSION_NOT_GRANTED_ERROR, "해당 질문글을 수정할 수 있는 권한이 없습니다");
+        }
+        question.update(questionDto.getTitle(), questionDto.getContent());
+        return questionId;
+    }
+
+    @Override
+    public void deleteQuestion(Long id, UserDetails userDetails) {
+        Optional<Question> optionalQuestion = questionRepository.findById(id);
+        if (optionalQuestion.isEmpty()) {
+            throw new PostNotFoundException(ErrorCode.POST_NOT_FOUND_ERROR, "해당하는 질문글이 존재하지 않습니다");
+        }
+        Question question = optionalQuestion.get();
+        String userid = question.getUser().getUserid();
+        if (!userid.equals(userDetails.getUsername())) {
+            throw new PermissionNotGrantedException(ErrorCode.PERMISSION_NOT_GRANTED_ERROR, "해당 질문글을 삭제할 수 있는 권한이 없습니다.");
+        }
+        questionRepository.delete(question);
+    }
+
+}
