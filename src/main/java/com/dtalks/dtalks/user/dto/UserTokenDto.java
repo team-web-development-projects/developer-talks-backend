e358f7ddb130f6c77c76c3f40c3a24ec1c5503cf
Harry Kim <39542757+rladydgn@users.noreply.github.com>
2023-05-04 23:44:03 +0900

    fix: token 관련 로직을 service로 옮김 (#29)

--- src/main/java/com/dtalks/dtalks/user/dto/UserTokenDto.java
+++ src/main/java/com/dtalks/dtalks/user/dto/UserTokenDto.java
@@ -2,9 +2,12 @@ package com.dtalks.dtalks.user.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UserTokenDto {
     private String email;
     private String userid;
     private String nickname;
+    List<String> roles;
 }
