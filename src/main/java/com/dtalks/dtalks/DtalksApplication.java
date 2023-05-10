427c23dcc00d0ba6f91ee448fa9f00fb6f62f189
Harry Kim <39542757+rladydgn@users.noreply.github.com>
2023-05-10 13:00:00 +0900

    fix: swagger request url 프로토콜 https로 변경 (#34)

--- src/main/java/com/dtalks/dtalks/DtalksApplication.java
+++ src/main/java/com/dtalks/dtalks/DtalksApplication.java
@@ -1,5 +1,7 @@
 package com.dtalks.dtalks;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
