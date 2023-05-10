51298c027d12ff2b8461b18984848ae4f0e0cde3
mohyerolo <garam2101@naver.com>
2023-05-10 18:50:42 +0900

    feat: post, comment swagger에 설명 추가

--- src/main/java/com/dtalks/dtalks/board/comment/controller/CommentController.java
+++ src/main/java/com/dtalks/dtalks/board/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.dtalks.dtalks.board.comment.controller;
 import com.dtalks.dtalks.board.comment.dto.CommentInfoDto;
 import com.dtalks.dtalks.board.comment.dto.CommentRequestDto;
 import com.dtalks.dtalks.board.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,39 +18,46 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+    @Operation(summary = "특정 댓글 조회")
     @GetMapping("/{id}")
     public ResponseEntity<CommentInfoDto> searchOneComment(@PathVariable Long id) {
         CommentInfoDto commentInfoDto = commentService.searchById(id);
         return ResponseEntity.ok(commentInfoDto);
     }
 
+    @Operation(summary = "특정 게시글의 댓글 리스트 조회")
     @GetMapping("/list/post/{postId}")
     public ResponseEntity<List<CommentInfoDto>> searchPostCommentList(@PathVariable Long postId) {
         List<CommentInfoDto> list = commentService.searchListByPostId(postId);
         return ResponseEntity.ok(list);
     }
 
+    @Operation(summary = "특정 유저의 댓글 리스트 조회")
     @GetMapping("/list/user/{userId}")
     public ResponseEntity<List<CommentInfoDto>> searchUserIdCommentList(@PathVariable Long userId) {
         List<CommentInfoDto> list = commentService.searchListByUserId(userId);
         return ResponseEntity.ok(list);
     }
 
+    @Operation(summary = "특정 게시글에 댓글 저장")
     @PostMapping("/{postId}")
     public void saveComment(@PathVariable Long postId, @Valid @RequestBody CommentRequestDto dto) {
         commentService.saveComment(postId, dto);
     }
 
+    @Operation(summary = "특정 게시글의 댓글에 대댓글 저장")
     @PostMapping("/{postId}/{parentId}")
     public void saveReComment(@PathVariable Long postId, @PathVariable Long parentId, @Valid @RequestBody CommentRequestDto dto) {
         commentService.saveReComment(postId, parentId, dto);
     }
 
+    @Operation(summary = "특정 댓글 수정")
     @PutMapping("/{id}")
     public void updateComment(@PathVariable Long id, @Valid @RequestBody CommentRequestDto dto) {
         commentService.updateComment(id, dto);
     }
 
+    @Operation(summary = "특정 댓글 삭제")
     @DeleteMapping("/{id}")
     public void deleteComment(@PathVariable Long id) {
         commentService.deleteComment(id);
