5b5c6c6cdb9726c7be7f350df68b66941eb0f380
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-05-12 20:29:50 +0900

    fix: swagger 토큰값 헤더에 넣을수 있게 함

--- src/main/java/com/dtalks/dtalks/base/config/OpenApiConfiguration.java
+++ src/main/java/com/dtalks/dtalks/base/config/OpenApiConfiguration.java
@@ -3,6 +3,8 @@ package com.dtalks.dtalks.base.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,9 +21,18 @@ public class OpenApiConfiguration {
         Info info = new Info()
                 .title("dtalks API Document")
                 .version("1.0.0");
+        String jwtName = "X-AUTH-TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtName, new SecurityScheme()
+                        .name(jwtName)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER));
+
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
-                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(info);
     }
 }
