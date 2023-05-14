13ebaf0a92de3fc592a3b212d969a5c1d672bb3e
Harry Kim <39542757+rladydgn@users.noreply.github.com>
2023-05-14 22:48:31 +0900

    feat: 스터디룸 리스트보기, 수정, 삭제 추가 (#46)

--- src/main/java/com/dtalks/dtalks/studyroom/repository/StudyRoomRepository.java
+++ src/main/java/com/dtalks/dtalks/studyroom/repository/StudyRoomRepository.java
@@ -1,8 +1,10 @@
 package com.dtalks.dtalks.studyroom.repository;
 
 import com.dtalks.dtalks.studyroom.entity.StudyRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyRoomRepository extends JpaRepository<StudyRoom, Long> {
-
+    Page<StudyRoom> findAll(Pageable pageable);
 }
