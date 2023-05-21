647aec24d197dcba4e8c2bf92fa409df404af966
Ramm <68698007+mohyerolo@users.noreply.github.com>
2023-05-21 20:33:05 +0900

    fix: 게시글 삭제시 즐겨찾기, 추천 테이블 검사 (#63)

--- src/main/java/com/dtalks/dtalks/board/post/repository/FavoritePostRepository.java
+++ src/main/java/com/dtalks/dtalks/board/post/repository/FavoritePostRepository.java
@@ -1,11 +1,16 @@
 package com.dtalks.dtalks.board.post.repository;
 
 import com.dtalks.dtalks.board.post.entity.FavoritePost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FavoritePostRepository extends JpaRepository<FavoritePost, Long> {
     Optional<FavoritePost> findByPostIdAndUserId(Long postId, Long userId);
+    Page<FavoritePost> findByUserId(Long userId, Pageable pageable);
+    List<FavoritePost> findByPostId(Long postId);
     boolean existsByPostIdAndUserId(Long postId, Long userId);
 }
