f4a0d6c407e4723dc92688603c237e6d9237d038
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-05-25 15:00:06 +0900

    feat: 스터디룸 가입신청 리스트, 가입승인 api

---
+++ src/main/java/com/dtalks/dtalks/studyroom/dto/StudyRoomJoinResponseDto.java
@@ -0,0 +1,45 @@
+package com.dtalks.dtalks.studyroom.dto;
+
+import com.dtalks.dtalks.studyroom.entity.StudyRoom;
+import com.dtalks.dtalks.studyroom.entity.StudyRoomUser;
+import com.dtalks.dtalks.studyroom.enums.Skill;
+import com.dtalks.dtalks.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class StudyRoomJoinResponseDto {
+
+    @Schema(description = "스터디룸 제목")
+    private String title;
+
+    @Schema(description = "스터디룸 id")
+    private Long studyRoomId;
+
+    @Schema(description = "스터디룸 유저 id")
+    private Long studyRoomUserId;
+
+    @Schema(description = "신청자 이름")
+    private String nickname;
+
+    @Schema(description = "신청자 내 소개")
+    private String description;
+
+    @Schema(description = "신청자 스킬")
+    private List<Skill> skills;
+
+    public static StudyRoomJoinResponseDto toDto(StudyRoom studyRoom, StudyRoomUser studyRoomUser, User user) {
+        return StudyRoomJoinResponseDto.builder()
+                .studyRoomId(studyRoom.getId())
+                .title(studyRoom.getTitle())
+                .studyRoomUserId(studyRoomUser.getId())
+                .nickname(user.getNickname())
+                .description(user.getDescription())
+                .skills(user.getSkills())
+                .build();
+    }
+}
