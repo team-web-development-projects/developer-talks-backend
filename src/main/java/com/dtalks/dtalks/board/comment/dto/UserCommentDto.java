fabe1cb6dc7bd7b20f4569096d20a97cbe0453c8
mohyerolo <garam2101@naver.com>
2023-06-02 21:00:04 +0900

    fix: Dto 빠진 파일 추가

---
+++ src/main/java/com/dtalks/dtalks/board/comment/dto/UserCommentDto.java
@@ -0,0 +1,43 @@
+package com.dtalks.dtalks.board.comment.dto;
+
+import com.dtalks.dtalks.board.comment.entity.Comment;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(description = "사용자의 댓글 리스트 DTO")
+public class UserCommentDto {
+    @Schema(description = "댓글 db에 저장된 id, primary key")
+    private Long id;
+
+    @Schema(description = "댓글을 단 게시글의 id, 게시글 조회할 때 보내는 것")
+    private Long postId;
+
+    @Schema(description = "댓글을 단 게시글의 제목")
+    private String postTitle;
+
+    @NotBlank
+    String content;
+
+    @Schema(description = "비밀글 여부")
+    boolean isSecret;
+
+    @Builder
+    public static UserCommentDto toDto(Comment comment, Long postId, String title) {
+        return UserCommentDto.builder()
+                .id(comment.getId())
+                .postId(postId)
+                .postTitle(title)
+                .content(comment.getContent())
+                .isSecret(comment.isSecret())
+                .build();
+    }
+}
