2d585f559c7be0c4d35f445fa1fc76f3b1a6e15a
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-06-30 17:58:30 +0900

    fix: 소켓 테스트

--- src/main/java/com/dtalks/dtalks/user/config/SecurityConfiguration.java
+++ src/main/java/com/dtalks/dtalks/user/config/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/sign-in/**", "/sign-up", "exception", "/users/check/**", "/email/**"
-                        ,"/token/refresh").permitAll()
+                        ,"/token/refresh", "ws/chat/**").permitAll()
                 .requestMatchers("**exception**").permitAll()
                 .requestMatchers("/users/recent/activity/**", "/users/private/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/post/**", "/comment/**").permitAll()
