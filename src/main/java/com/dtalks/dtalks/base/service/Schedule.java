be19741354a8ce9cb469b89b85d0a2703458ae2d
mohyerolo <garam2101@naver.com>
2023-07-05 14:50:44 +0900

    fix: 알림 삭제 스케줄 등록

--- src/main/java/com/dtalks/dtalks/base/service/Schedule.java
+++ src/main/java/com/dtalks/dtalks/base/service/Schedule.java
@@ -1,8 +1,9 @@
 package com.dtalks.dtalks.base.service;
 
+import com.dtalks.dtalks.notification.enums.ReadStatus;
+import com.dtalks.dtalks.notification.repository.NotificationRepository;
 import com.dtalks.dtalks.user.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,14 +14,14 @@ import java.time.LocalDateTime;
 @Service
 @RequiredArgsConstructor
 public class Schedule {
-
-    @Autowired
     private final ActivityRepository activityRepository;
+    private final NotificationRepository notificationRepository;
 
     @Scheduled(cron = "0 0 0 1 * ?")
     @Transactional
     @Async
-    public void activityDeleteSchedule() {
+    public void deleteSchedule() {
         activityRepository.deleteByCreateDateLessThan(LocalDateTime.now().minusMonths(1));
+        notificationRepository.deleteByCreateDateLessThanAndReadStatus(LocalDateTime.now().minusMonths(1), ReadStatus.READ);
     }
 }
