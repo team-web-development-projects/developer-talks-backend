83712c8ffd682c9bdcbade30e66670829f224c9b
mohyerolo <garam2101@naver.com>
2023-07-05 18:34:47 +0900

    fix: 각 활동에서 기간별 데이터 가져오도록 수정

--- src/main/java/com/dtalks/dtalks/board/comment/repository/CommentRepository.java
+++ src/main/java/com/dtalks/dtalks/board/comment/repository/CommentRepository.java
@@ -1,12 +1,15 @@
 package com.dtalks.dtalks.board.comment.repository;
 
 import com.dtalks.dtalks.board.comment.entity.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     boolean existsByPostId(Long postId);
     List<Comment> findByPostId(Long postId);
     List<Comment> findByUserIdAndRemovedFalse(Long userId);
+    List<Comment> findByUserIdAndCreateDateBetween(Long userId, LocalDateTime goe, LocalDateTime loe);
 }
--- src/main/java/com/dtalks/dtalks/qna/answer/repository/AnswerRepository.java
+++ src/main/java/com/dtalks/dtalks/qna/answer/repository/AnswerRepository.java
@@ -3,10 +3,11 @@ package com.dtalks.dtalks.qna.answer.repository;
 import com.dtalks.dtalks.qna.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByQuestionId(Long id);
-
+    List<Answer> findByUserIdAndCreateDateBetween(Long userId, LocalDateTime goe, LocalDateTime loe);
     List<Answer> findByUserId(Long id);
 }
