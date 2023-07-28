e4419120c3ce2bdbdac92d45273aadf6bd9f5a58
mohyerolo <garam2101@naver.com>
2023-07-28 22:09:40 +0900

    update: 신고 취소, 사용자가 내역 조회하는 기능 삭제, 신고당한 사용자에게 알림 추가

--- src/main/java/com/dtalks/dtalks/report/service/ReportUserService.java
+++ src/main/java/com/dtalks/dtalks/report/service/ReportUserService.java
@@ -1,14 +1,7 @@
 package com.dtalks.dtalks.report.service;
 
-import com.dtalks.dtalks.report.dto.ReportDetailDto;
 import com.dtalks.dtalks.report.dto.ReportDetailRequestDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface ReportUserService {
-    Page<ReportDetailDto> searchAllUserReports(Pageable pageable);
-
     void report(String nickname, ReportDetailRequestDto dto);
-    void cancelReport(Long id);
-
 }
