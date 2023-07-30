9d1f43aa58450e89790801967f52fb6d627dbf99
mohyerolo <garam2101@naver.com>
2023-07-30 15:06:51 +0900

    feat: 전체 회원 조회 controller 추가

---
+++ src/main/java/com/dtalks/dtalks/admin/user/dto/UserManageDto.java
@@ -0,0 +1,37 @@
+package com.dtalks.dtalks.admin.user.dto;
+
+import com.dtalks.dtalks.user.entity.User;
+import com.dtalks.dtalks.user.enums.ActiveStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserManageDto {
+    private Long id;
+    private String userid;
+    private String email;
+    private String nickname;
+    private Boolean isActive;
+    private ActiveStatus status;
+    private LocalDateTime createDate;
+    private LocalDateTime modifiedDate;
+
+    public static UserManageDto toDto(User user) {
+        return UserManageDto.builder()
+                .id(user.getId())
+                .userid(user.getUserid())
+                .nickname(user.getNickname())
+                .isActive(user.getIsActive())
+                .status(user.getStatus())
+                .createDate(user.getCreateDate())
+                .modifiedDate(user.getModifiedDate())
+                .build();
+    }
+}
