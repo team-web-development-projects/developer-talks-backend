7e8304bd34424ea04aa75fde47f3d162ef2dc195
mohyerolo <garam2101@naver.com>
2023-08-03 13:30:57 +0900

    update: 신고 사용자 dto 수정

--- src/main/java/com/dtalks/dtalks/admin/report/dto/ReportedUserDto.java
+++ src/main/java/com/dtalks/dtalks/admin/report/dto/ReportedUserDto.java
@@ -14,14 +14,14 @@ import lombok.*;
 @Schema(description = "신고 당한 사용자의 정보와 처리 내용")
 public class ReportedUserDto {
     @Schema(description = "신고 당한 사용자 정보")
-    private UserManageDto userManageDto;
+    private ReportedUserInfoDto reportedUserInfoDto;
 
     @Schema(description ="처리 결과: WAIT(처리 전), SUSPENSION(계정 일시 정지), BAN(계정 영구 정지 - 탈퇴와 같은 효과), NP(문제 없음)")
     private ResultType resultType;
 
     public static ReportedUserDto toDto(User reportedUser) {
         return ReportedUserDto.builder()
-                .userManageDto(UserManageDto.toDto(reportedUser))
+                .reportedUserInfoDto(ReportedUserInfoDto.toDto(reportedUser))
                 .resultType(ResultType.WAIT)
                 .build();
     }
