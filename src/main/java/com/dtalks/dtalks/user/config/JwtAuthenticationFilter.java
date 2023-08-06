a175eb0af1ea7458bb099a70f646851235cc2e18
mohyerolo <garam2101@naver.com>
2023-08-06 17:53:19 +0900

    fix: 정지 계정의 접근 시 에러 catch문 작성 (403)

--- src/main/java/com/dtalks/dtalks/user/config/JwtAuthenticationFilter.java
+++ src/main/java/com/dtalks/dtalks/user/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.dtalks.dtalks.user.config;
 
+import com.dtalks.dtalks.exception.exception.CustomException;
 import com.dtalks.dtalks.user.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,12 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -37,11 +38,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 //        }
         String token = tokenService.resolveToken(request);
 
-        if(token != null && tokenService.validateToken(token)) {
-            Authentication authentication = tokenService.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
+        try {
+            if(token != null && tokenService.validateToken(token)) {
+                Authentication authentication = tokenService.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
 
-        filterChain.doFilter(request, response);
+            filterChain.doFilter(request, response);
+        } catch (CustomException c) {
+            response.setStatus(c.getErrorCode().getStatus());
+            response.setContentType(String.valueOf(MediaType.APPLICATION_JSON));
+            response.setCharacterEncoding("UTF-8");
+
+            response.getWriter().write(c.getMessage());
+        }
     }
 }
