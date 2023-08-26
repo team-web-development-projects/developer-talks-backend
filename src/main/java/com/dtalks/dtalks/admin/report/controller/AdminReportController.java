f2030cc5ab0bb49e4a0bf936cb3c510d5388cb2d
mohyerolo <garam2101@naver.com>
2023-08-26 15:54:41 +0900

    feat:admin 게시글 신고 처리 추가 및 기존 신고 처리 방법 수정

--- src/main/java/com/dtalks/dtalks/admin/report/controller/AdminReportController.java
+++ src/main/java/com/dtalks/dtalks/admin/report/controller/AdminReportController.java
@@ -1,7 +1,9 @@
 package com.dtalks.dtalks.admin.report.controller;
 
 import com.dtalks.dtalks.admin.report.dto.ReportDetailDto;
+import com.dtalks.dtalks.admin.report.dto.ReportedPostDto;
 import com.dtalks.dtalks.admin.report.dto.ReportedUserDto;
+import com.dtalks.dtalks.admin.report.enums.DType;
 import com.dtalks.dtalks.admin.report.service.AdminReportService;
 import com.dtalks.dtalks.report.enums.ResultType;
 import io.swagger.v3.oas.annotations.Operation;
@@ -35,9 +37,24 @@ public class AdminReportController {
         return ResponseEntity.ok(adminReportService.getAllNotProgressedReportsByUser(reportedUserId, pageable));
     }
 
-    @Operation(summary = "신고 처리, resultType=\"BAN / SUSPENSION / NP\"")
-    @PutMapping("/user/{reportedUserId}/{resultType}")
-    public ResponseEntity<ResultType> handleReport(@PathVariable Long reportedUserId, @PathVariable ResultType resultType) {
-        return ResponseEntity.ok(adminReportService.handleReport(reportedUserId, resultType));
+    @Operation(summary = "신고된 게시글들 보기")
+    @GetMapping("/post/all")
+    public ResponseEntity<Page<ReportedPostDto>> searchAllNotProgressedPostReports(
+            @PageableDefault(size = 10, sort = "id",  direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(adminReportService.searchAllNotProgressedPostReports(pageable));
+    }
+
+    @Operation(summary = "신고된 게시글에게 해당하는 신고 내역 보기 (size=10, sort=id,desc 적용)")
+    @GetMapping("/post")
+    public ResponseEntity<Page<ReportDetailDto>> getAllNotProgressedReportsByPost(
+            @RequestParam Long reportedPostId,
+            @PageableDefault(size = 10, sort = "id",  direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(adminReportService.getAllNotProgressedReportsByPost(reportedPostId, pageable));
+    }
+
+    @Operation(summary = "신고된 사용자 또는 게시글 처리", description = "사용자일 경우에는 BAN, SUSPENSION, NP / 게시글은 NP, FORBIDDEN")
+    @PutMapping("/handle/{dtype}")
+    public ResponseEntity<ResultType> handleReports(@PathVariable DType dtype, @RequestParam Long reportedObjectId, @RequestParam ResultType resultType) {
+        return ResponseEntity.ok(adminReportService.handleReports(dtype, reportedObjectId, resultType));
     }
 }
--- src/main/java/com/dtalks/dtalks/report/repository/ReportedPostRepository.java
+++ src/main/java/com/dtalks/dtalks/report/repository/ReportedPostRepository.java
@@ -1,9 +1,16 @@
 package com.dtalks.dtalks.report.repository;
 
 import com.dtalks.dtalks.report.entity.ReportedPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ReportedPostRepository extends JpaRepository<ReportedPost, Long> {
     boolean existsByProcessedFalseAndDtypeAndReportUserIdAndPostId(String dtype, Long reportUserId, Long reportedUserId);
+    Page<ReportedPost> findByProcessedFalseAndPostId(Long postId, Pageable pageable);
 
+    List<ReportedPost> findByProcessedFalseAndPostIdAndCreateDateLessThan(Long postId, LocalDateTime createDate);
 }
