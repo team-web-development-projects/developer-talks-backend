8890b9c9ffa26332421febb12be2ea061a1a3eb4
mohyerolo <garam2101@naver.com>
2023-08-26 15:49:38 +0900

    update:admin 신고관리 api 변경

--- src/main/java/com/dtalks/dtalks/admin/report/controller/AdminReportController.java
+++ src/main/java/com/dtalks/dtalks/admin/report/controller/AdminReportController.java
@@ -15,22 +15,22 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/report")
+@RequestMapping("/admin/reports")
 public class AdminReportController {
 
     private final AdminReportService adminReportService;
 
     @Operation(summary = "신고가 들어온 사용자들 페이지로 보기 (size=10, sort=id,desc 적용)")
-    @GetMapping("/all")
+    @GetMapping("/user/all")
     public ResponseEntity<Page<ReportedUserDto>> searchAllNotProgressedUserReports(
             @PageableDefault(size = 10, sort = "id",  direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(adminReportService.searchAllNotProgressedUserReports(pageable));
     }
 
     @Operation(summary = "신고된 사용자에게 해당하는 신고 내역 보기 (size=10, sort=id,desc 적용)")
-    @GetMapping("/user/{reportedUserId}")
+    @GetMapping("/user")
     public ResponseEntity<Page<ReportDetailDto>> getAllNotProgressedReportsByUser(
-            @PathVariable Long reportedUserId,
+            @RequestParam Long reportedUserId,
             @PageableDefault(size = 10, sort = "id",  direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(adminReportService.getAllNotProgressedReportsByUser(reportedUserId, pageable));
     }
