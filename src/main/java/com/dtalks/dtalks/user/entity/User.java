1ef05662f1837671a2404e3451c02152ef476d6d
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-08-29 09:54:53 +0900

    fix: user skills, roles fetch lazy로 변경, dto로 변경시 강제 로딩하도록 수정

--- src/main/java/com/dtalks/dtalks/user/entity/User.java
+++ src/main/java/com/dtalks/dtalks/user/entity/User.java
@@ -56,11 +56,11 @@ public class User extends BaseTimeEntity implements UserDetails{
 
     private String registrationId;
 
-    @ElementCollection(fetch = FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.LAZY)
     @Builder.Default // 빌더 패턴을 통해 인스턴스를 만들 때 특정 필드값으로 초기화
     private List<String> roles = new ArrayList<>();
 
-    @ElementCollection(fetch = FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.LAZY)
     @Builder.Default
     private List<Skill> skills = new ArrayList<>();
 
